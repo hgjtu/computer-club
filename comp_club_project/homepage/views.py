324095113a9bf0a626caac84d243bad62132b8d8
MK <vlrtmsq@gmail.com>
2024-12-05 01:35:07 +0300

    главная страницы

--- comp_club_project/homepage/views.py
+++ comp_club_project/homepage/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 
+component_list = [
+    {
+        "title": "Ебать какая видюха",   # Видеокарты GeForce RTX 4070 Ti
+        "img": "img/homepage/video_card.jpg",
+    },
+    {
+        "title": "Мать в канаве?",   # Материнские платы MSI
+        "img": "img/homepage/motherboard.jpg",
+    },
+    {
+        "title": "Проццц?",   # Процессоры Intel Core i7 и AMD Ryzen 9
+        "img": "img/homepage/CPU.jpg",
+    },
+]
+
+
+games = [
+    '1', '2', '3', '4', '5'
+]
+
 
 def index(request):
     template = "homepage/index.html"
-    return render(request, template)
+    context = {'component_list': component_list,
+               'games_count': len(games)}
+    return render(request, template, context=context)
