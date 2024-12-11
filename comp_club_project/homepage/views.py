62808faeae4d2c86fde960952a3a7e3dde66d5fb
MK <vlrtmsq@gmail.com>
2024-12-11 13:05:26 +0300

    для курсача пойдет

--- comp_club_project/homepage/views.py
+++ comp_club_project/homepage/views.py
@@ -1,28 +1,33 @@
 from django.shortcuts import render
+from django.db import connection
 
 component_list = [
     {
-        "title": "Ебать какая видюха",   # Видеокарты GeForce RTX 4070 Ti
+        "title": "Видеокарты GeForce RTX 4070 Ti",
         "img": "img/homepage/video_card.jpg",
     },
     {
-        "title": "Мать в канаве?",   # Материнские платы MSI
+        "title": "Материнские платы MSI",
         "img": "img/homepage/motherboard.jpg",
     },
     {
-        "title": "Проццц?",   # Процессоры Intel Core i7 и AMD Ryzen 9
+        "title": "Процессоры Intel Core i7 и AMD Ryzen 9",
         "img": "img/homepage/CPU.jpg",
     },
 ]
 
 
-games = [
-    '1', '2', '3', '4', '5'
-]
-
-
 def index(request):
     template = "homepage/index.html"
+    games = set()
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT installed_apps_and_games \
+            FROM services_equipment_equipment"
+            )
+        rows = cursor.fetchall()
+    for row in rows:
+        games.update(row[0].split(", "))
     context = {'component_list': component_list,
                'games_count': len(games)}
     return render(request, template, context=context)
